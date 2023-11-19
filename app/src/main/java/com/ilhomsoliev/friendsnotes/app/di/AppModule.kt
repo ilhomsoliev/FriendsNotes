dcaf8f588fb8e0689e0cdee926f86f3ad9d73914
Ilhom Soliev <soliev.ilhom.2005@gmail.com>
2023-11-19 20:24:13 +0300

    comment afadsfasd f

--- app/src/main/java/com/ilhomsoliev/friendsnotes/app/di/AppModule.kt
+++ app/src/main/java/com/ilhomsoliev/friendsnotes/app/di/AppModule.kt
@@ -10,7 +10,7 @@ import org.koin.dsl.module
 fun appModule() = module {
     single { getDatabaseInstance(androidContext()) }
     single<PersonDao> { get<ApplicationDatabase>().personDao() }
-}//jnjknkjnjk asd adsfadsf
+}//jnjknkjnjk asd adsfadsf asdf adsf
 
 fun dataStore() = module {
     single { DataStoreManager(androidContext()) }
